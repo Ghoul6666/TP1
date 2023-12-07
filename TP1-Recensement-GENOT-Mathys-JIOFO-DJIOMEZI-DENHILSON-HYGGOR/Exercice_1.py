9ae3a772361521273e86a28ce70a5285dd726304
Ghoul6666 <mathys.genot@outlook.com>
2023-12-07 16:20:16 +0100

    TP_1 Exercice1

--- TP1-Recensement-GENOT-Mathys-JIOFO-DJIOMEZI-DENHILSON-HYGGOR/Exercice_1.py
+++ TP1-Recensement-GENOT-Mathys-JIOFO-DJIOMEZI-DENHILSON-HYGGOR/Exercice_1.py
@@ -1,1 +1,34 @@
-a
+import csv
+table=[]
+cal=[]
+
+with open('donnees_2008.csv', newline="") as csvfile:
+    reader=csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        table.append(row)
+    #print(table)
+    for row in table :
+        if row[6] == 'Auxerre':
+            cal.append(row[9])
+
+
+with open('donnees_2016.csv', newline="") as csvfile:
+    reader=csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        table.append(row)
+    #print(table)
+    for row in table :
+        if row[6] == 'Auxerre':
+            cal.append(row[9])
+print("Le nombre de population a Auxerre en 2008 et 2016 :",cal[0]+cal[1],"de population.")
+'''
+with open('donnees_2021.csv', newline="") as csvfile:
+    reader=csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        table.append(row)
+    #print(table)
+    for row in table :
+        if row[6] == 'Auxerre':
+            cal.append(row[9])
+print(cal)
+'''
